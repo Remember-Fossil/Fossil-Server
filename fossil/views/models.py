34905ee78fda55fd204aa7324428e49b4163bd98
Jang Junha <junha113@daum.net>
2015-03-21 17:13:44 +0900

    add example models

---
+++ fossil/views/models.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from google.appengine.ext import db
+
+
+# class User(db.Model):
+#    email = db.StringProperty()
+#    password = db.StringProperty()
+#    name = db.StringProperty()
+
+#    peach = db.IntegerProperty(default=0)
+
+#    signup_datetime = db.DateTimeProperty(auto_now_add=True)
+
+#    friends = db.ListProperty(db.Key)
+
