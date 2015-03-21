b27d2e8d53a2ce6a54cadb7bd0a15dd670facbe3
iLoveChocolate <erfdcx0413@gmail.com>
2015-03-21 16:53:15 +0900

    config fossil, __init__

--- appengine_config.py
+++ appengine_config.py
@@ -5,4 +5,4 @@ import inspect
 # add `lib` subdirectory to `sys.path`, so our `main` module can load
 # third-party libraries.
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib'))
-sys.path.insert(0, os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"modules"))))
+sys.path.insert(0, os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"fossil"))))
