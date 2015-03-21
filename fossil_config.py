4bde35201311f695a78fda0c2e499efa7382b6ea
iLoveChocolate <erfdcx0413@gmail.com>
2015-03-21 19:49:33 +0900

    facebook redirect url

--- fossil_config.py
+++ fossil_config.py
@@ -5,9 +5,9 @@ SECRET_KEY = 'Development-key'
 
 
 if DEBUG:
-    FACEBOOK_REDIRECT_URL = 'http://localhost:8080'
+    FACEBOOK_REDIRECT_URL = 'http://localhost:8080/login'
 else:
-    FACEBOOK_REDIRECT_URL = 'remember-fossil.appspot.com'
+    FACEBOOK_REDIRECT_URL = 'remember-fossil.appspot.com/login'
 
 FACEBOOK_APP_ID = '407255472781563'
 FACEBOOK_APP_SECRET = '45f3dc347e3721f0d4cf43e159d69c3c'
